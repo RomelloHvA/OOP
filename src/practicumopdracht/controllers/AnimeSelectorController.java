38015c5cade0cc62d4cff288e2caa21ee6cc0f9a
Romello <romello.ten.broeke@hva.nl>
2023-03-03 23:12:24 +0100

    Eerste twee velden worden correct afgehandeld met error messages

--- src/practicumopdracht/controllers/AnimeSelectorController.java
+++ src/practicumopdracht/controllers/AnimeSelectorController.java
@@ -9,13 +9,22 @@ import practicumopdracht.MainApplication;
 import practicumopdracht.views.AnimeSelectorView;
 import practicumopdracht.views.View;
 
+import java.time.LocalDate;
+
 public class AnimeSelectorController extends Controller{
 
     private AnimeSelectorView animeSelectorView;
     private ReviewController reviewController;
+    private Alert alert;
+    private String errorMessage;
+    private Alert confirmNewAnimeAlert;
 
     public AnimeSelectorController() {
         this.animeSelectorView = new AnimeSelectorView();
+        this.alert = new Alert(Alert.AlertType.WARNING);
+        this.confirmNewAnimeAlert = new Alert(Alert.AlertType.CONFIRMATION,"Confirm new Anime?");
+        errorMessage = "please enter valid\n";
+
         this.animeSelectorView.getReviewsButton().setOnMouseClicked(mouseEvent -> handleReviewButtonClick());
         this.animeSelectorView.getNewButton().setOnMouseClicked(mouseEvent -> handleAddNewAnime());
     }
@@ -31,20 +40,54 @@ public class AnimeSelectorController extends Controller{
     }
 
     private void handleAddNewAnime(){
-        String errorMessage = "Please enter: \n";
-        if (isEmptyAnimeName()){
-            errorMessage += "Anime name";
-            Alert nameEmpty = new Alert(Alert.AlertType.ERROR, errorMessage);
-            animeSelectorView.getAnimeNameTextField().setBorder(Border.stroke(Color.ORANGE));
-            nameEmpty.show();
-        }
-        else {
-            Alert nameGood = new Alert(Alert.AlertType.CONFIRMATION,"Confirm new Anime?");
-            nameGood.show();
+        boolean emptyAnimeName = isEmptyAnimeName();
+        boolean validReleaseDate = isValidReleaseDate();
+
+        if (emptyAnimeName || validReleaseDate == false){
+            alert.setContentText(errorMessage);
+            alert.show();
+            errorMessage = "Please enter valid:\n";
+        } else {
+            confirmNewAnimeAlert.show();
         }
 
-    }
+
+        }
+
     private boolean isEmptyAnimeName(){
-        return isEmptyTextField(animeSelectorView.getAnimeNameTextField());
+        boolean isEmpty = isEmptyTextField(animeSelectorView.getAnimeNameTextField());
+        if (isEmpty){
+            addErrorMessageAnimeName();
+        }
+        return isEmpty;
+    }
+    private boolean isValidReleaseDate(){
+        LocalDate releaseDateInput = animeSelectorView.getReleaseDatePicker().getValue();
+        boolean isValidReleaseDate = false;
+
+        if (releaseDateInput == null){
+            addErrorMessageReleaseDate();
+            return false;
+        }
+
+        if (!releaseDateInput.isBefore(MIN_ANIME_RELEASEDATE) || !releaseDateInput.isAfter(CURRENT_DATE)){
+            isValidReleaseDate = true;
+        }
+
+
+        if (!isValidReleaseDate){
+            addErrorMessageReleaseDate();
+        }
+
+        return isValidReleaseDate;
+    }
+
+    private void addErrorMessageReleaseDate(){
+        errorMessage += "Release date\n";
+        animeSelectorView.getReleaseDatePicker().setBorder(Border.stroke(Color.ORANGE));
+    }
+    private void addErrorMessageAnimeName(){
+        errorMessage += "Anime name\n";
+        animeSelectorView.getAnimeNameTextField().setBorder(Border.stroke(Color.ORANGE));
     }
 }
--- src/practicumopdracht/controllers/Controller.java
+++ src/practicumopdracht/controllers/Controller.java
@@ -11,7 +11,7 @@ public abstract class Controller implements EventListener {
     protected final double MAX_RATING = 5.0;
     protected final double MIN_RATING = 0;
     protected final LocalDate MIN_ANIME_RELEASEDATE = LocalDate.ofEpochDay(30-5-1917);
-    protected final LocalDate MAX_WRITEDATE = LocalDate.now();
+    protected final LocalDate CURRENT_DATE = LocalDate.now();
     protected final int MIN_EPISODES = 1;
 
 
